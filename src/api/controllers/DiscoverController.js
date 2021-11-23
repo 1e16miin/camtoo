6c2f307f7c7a0447f9e841be67eb07b9d9b10125
1e16miin <ddogi1513@naver.com>
2021-11-24 00:26:55 +0900

    1124_00:26

--- src/api/controllers/DiscoverController.js
+++ src/api/controllers/DiscoverController.js
@@ -5,4 +5,17 @@ const express = require('express')
 const router = express()
 
 
+router.get('/geo-fence', async (req,res) => {
+  
+  try {
+    const geoFenceInstance = GeoFenceService(1)
+    const result = geoFenceInstance.getBuildings(1)
+    return res.status(200).send(result)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({error:"데이터를 찾는 과정에 오류가 발생하였습니다."})
+  }
+})
+
+
 module.exports = router
--- src/services/GeoFenceService.js
+++ src/services/GeoFenceService.js
@@ -1,16 +1,48 @@
-const { Building } = require("../models");
+const { boundary, building, member, friend, user } = require("../models");
+const {Op} = require('sequelize')
+
 
 const GeoFenceService = (universityId) => {
-  const getBuildings = async () => {
-    const buildingIdData = await Building.findAll({
+  const getBuildings = async (profileId = 1) => {
+    
+    let buildingData = (await building.findAll({
       nest: true,
       raw: true,
       where: { university_id: universityId },
-      include: [{
-        model: Boundary
-      }],
-    });
+      attributes: [["id", "building_id"], "x", "y"]
+    }))
+    
+    const friendIds = (await friend.findAll({ nest: true, raw: true, attributes:["user.id"] ,where:{status:"B"}, include:[{model:user, where:{profile_id:profileId}}]})).map(element=>element.id)
+
+    buildingData = await Promise.all(buildingIdList.map(building => {
+      const boundaryData = await boundary.findAll({
+        nest: true,
+        raw: true,
+        attributes: ["x", "y"],
+        where: { building_id: building.building_id },
+      });
+      const isIn = await member.findAll({
+        nest: true,
+        raw: true,
+        attributes: ["id"],
+        where: { profile_id: friendIds },
+        // include: [{ model: friend, attributes: [], required: true, where: { status: "A" },include:[{model:user, where:{profile_id:profileId}}] }],
+      });
+      
+      building.boundaries = boundaryData
+      const marker = { x: building.x, y: building.y };
+      building.marker = marker;
+      building.isEmpty = isIn ? 1 : 0
+      delete building.x
+      delete building.y
+      return building
+    }));
+    
+
+
+    return buildingData
   };
+  return {getBuildings}
 };
 
 module.exports = GeoFenceService;
