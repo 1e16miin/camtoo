c91678b6af81682b44bc53efaca0c9e60c705793
1e16miin <ddogi1513@naver.com>
2021-11-26 20:12:04 +0900

    1126_20:09

--- src/api/middlewares/verifyToken.js
+++ src/api/middlewares/verifyToken.js
@@ -5,7 +5,7 @@ module.exports = {
   async checkAccessTokens(req, res, next) {
 
     const { authorization } = req.headers;
-    console.log(req.header);
+    console.log(req.headers);
     const credentials = authorization.replace("Bearer ", "");
 
     const tokenData = verifyToken(credentials);
