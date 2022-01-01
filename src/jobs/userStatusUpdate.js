3acee0663220c7f1d1e695a32a66844cb7931735
1e16miin <ddogi1513@naver.com>
2022-01-01 12:15:36 +0900

    fix userStatusUpdate Scheduler

--- src/jobs/userStatusUpdate.js
+++ src/jobs/userStatusUpdate.js
@@ -10,48 +10,50 @@ let rule = new schedule.RecurrenceRule()
 rule.minute = new schedule.Range(0,59,30)
 
 const userStatusUpdate = () => schedule.scheduleJob(rule, async () => {
-  const day = moment().day()
-  const hour = moment().hour()
-  const time = moment().minute() + hour * 60
-  
-  let transaction = await sequelize.transaction();
-   try {
-     const allUsers = await user.findAll({
-       nest: true,
-       raw: true,
-       attributes: ["user_id", "time_tables.class_type", "status"],
-       include: [
-         {
-           model: timeTable,
-           where: {
-             start_time: { [Op.lte]: time },
-             end_time: { [Op.gte]: time },
-             day_of_the_week: day - 1,
-           },
-           attributes:[],
-           required: false,
-         },
-       ],
-     });
-     await Promise.all(
-       allUsers.map(async (userData) => {
-         let status = 2;
-         if (userData.class_type) status = userData.class_type;
-         await user.update(
-           { status: status },
-           { where: { user_id: userData.user_id }, transaction }
-         );
-       })
-     );
-     await transaction.commit()
-     console.log("스케쥴링 완료")
-   } catch (err) {
-     console.log("스케쥴링 에러");
-     await transaction.rollback();
-   }
+  if (process.env.INSTANCE_ID === '0') {
+    const day = moment().day();
+    const hour = moment().hour();
+    const time = moment().minute() + hour * 60;
+
+    let transaction = await sequelize.transaction();
+    try {
+      const allUsers = await user.findAll({
+        nest: true,
+        raw: true,
+        attributes: ["user_id", "time_tables.class_type", "status"],
+        include: [
+          {
+            model: timeTable,
+            where: {
+              start_time: { [Op.lte]: time },
+              end_time: { [Op.gte]: time },
+              day_of_the_week: day - 1,
+            },
+            attributes: [],
+            required: false,
+          },
+        ],
+      });
+      await Promise.all(
+        allUsers.map(async (userData) => {
+          let status = 2;
+          if (userData.class_type) status = userData.class_type;
+          await user.update(
+            { status: status },
+            { where: { user_id: userData.user_id }, transaction }
+          );
+        })
+      );
+      await transaction.commit();
+      console.log("스케쥴링 완료");
+    } catch (err) {
+      console.log("스케쥴링 에러");
+      await transaction.rollback();
+    }
     
+  }
+  
 })
 
 
 module.exports = { userStatusUpdate }
-
