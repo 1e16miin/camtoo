cbda724d3c539004473d60a5f4dcf55ed46a02c6
1e16miin <ddogi1513@naver.com>
2022-01-02 22:01:56 +0900

    feat: imageUpload

--- src/utils/createSignedUrl.js
+++ src/utils/createSignedUrl.js
@@ -1,30 +1,30 @@
-const moment = require("moment-timezone");
-moment().tz("Asia/Seoul");
+// const moment = require("moment-timezone");
+// moment().tz("Asia/Seoul");
 
-const { awsconfig, cf_replaced_domain } = require("../keys");
+// const { awsconfig, cf_replaced_domain } = require("../keys");
 
-const cf = require("aws-cloudfront-sign");
+// const cf = require("aws-cloudfront-sign");
 
-const createSignedUrl = (route, uuid, type = "") => {
+// const createSignedUrl = (route, uuid, type = "") => {
   
-    let signedUrl = null;
-    if (uuid) {
-      let signingParams = {
-        keypairId: awsconfig.cloudfrontaccesskey,
-        privateKeyString: awsconfig.cloudfrontsecretkey,
-        // Optional - this can be used as an alternative to privateKeyString
-        // privateKeyPath: '/path/to/private/key',
-        expireTime: moment().add(1, "hours"),
-      };
+//     let signedUrl = null;
+//     if (uuid) {
+//       let signingParams = {
+//         keypairId: awsconfig.cloudfrontaccesskey,
+//         privateKeyString: awsconfig.cloudfrontsecretkey,
+//         // Optional - this can be used as an alternative to privateKeyString
+//         // privateKeyPath: '/path/to/private/key',
+//         expireTime: moment().add(1, "hours"),
+//       };
 
-      signedUrl = cf.getSignedUrl(
-        `https://${cf_replaced_domain}/${route}/${uuid}${type}`,
-        signingParams
-      );
-    }
+//       signedUrl = cf.getSignedUrl(
+//         `https://${cf_replaced_domain}/${route}/${uuid}${type}`,
+//         signingParams
+//       );
+//     }
 
-    return signedUrl;
+//     return signedUrl;
  
-};
+// };
 
-module.exports = createSignedUrl;
+// module.exports = createSignedUrl;
