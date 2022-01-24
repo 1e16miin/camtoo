ff65534553be204dce1aaaba1e206c6a97f27f5b
1e16miin <ddogi1513@naver.com>
2022-01-24 22:08:44 +0900

    fix delete user

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -108,7 +108,7 @@ const AuthService = () => {
   const deleteUser = async (id) =>{
     try{
       console.log(id)
-      await user.delete({where:{id:id}})
+      await user.destroy({where:{id:id}})
     }catch(err){
       throw new Error("유저 삭제 에러");
     }
