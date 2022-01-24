92612233deb97f638d0ac51f7260134058b71ab8
1e16miin <ddogi1513@naver.com>
2022-01-24 23:00:38 +0900

    chore add console.log

--- src/api/controllers/AuthController.js
+++ src/api/controllers/AuthController.js
@@ -11,6 +11,7 @@ router.post("/sign-up", async (req, res) => {
   try {
     const signUpData = req.body
     const authInstance = AuthService()
+    console.log(signUpData)
     const result = await authInstance.createNewUser(signUpData)
     return res.status(200).send(result)
   } catch (err) {
