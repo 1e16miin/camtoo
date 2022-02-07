5e46b8d1386da1875093ef16cf21765900a3f78b
1e16miin <ddogi1513@naver.com>
2022-02-08 07:49:42 +0900

    fix confirm verify code

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,15 +1,23 @@
-const { sequelize, user } = require("../models");
-const { jwtSecretKey, master } = require("../config/key");
+const {
+  sequelize,
+  user
+} = require("../models");
+const {
+  jwtSecretKey,
+  master
+} = require("../config/key");
 const jwt = require("jsonwebtoken");
 const TimeTableService = require("./TimeTableService");
 const pm2ClusterCache = require("pm2-cluster-cache");
 const NotificationService = require("./NotificationService");
 
-let cache = pm2ClusterCache.init({ storage: "all" });
+let cache = pm2ClusterCache.init({
+  storage: "all"
+});
 
 const AuthService = () => {
-  
- const createVerifyCode = (n = 4) => {
+
+  const createVerifyCode = (n = 4) => {
     let str = "";
     for (let i = 0; i < n; i++) {
       str += Math.floor(Math.random() * 10);
@@ -18,7 +26,7 @@ const AuthService = () => {
   };
 
   const sendVerifyCode = async (receiver) => {
-    
+
     try {
       const verifyCode = createVerifyCode();
       const notificationInstance = NotificationService();
@@ -30,61 +38,89 @@ const AuthService = () => {
       console.log(err);
       throw err;
     }
-  }; 
-  
-  const confirmVerifyCode = async (authData) => {
-    const { phoneNumber, verifyCode, encryptedPhoneNumber } = authData;
- 
-		let result = {}
-	
-		const cacheData = await cache.get(phoneNumber);
+  };
 
-		if (phoneNumber !== master) {
+  const confirmVerifyCode = async (authData) => {
+    const {
+      phoneNumber,
+      verifyCode,
+      encryptedPhoneNumber
+    } = authData;
+
+    let result = {}
+
+    const cacheData = await cache.get(phoneNumber);
+
+    if (phoneNumber !== master) {
 
       console.log(cacheData, phoneNumber, verifyCode)
-					if (!cacheData) {
-						throw new Error("제한 시간이 초과하였습니다");
-					}
+      if (!cacheData) {
+        throw new Error("제한 시간이 초과하였습니다");
+      }
 
-					if (cacheData !== verifyCode) {
-						throw new Error("인증 번호가 맞지 않습니다.");
-					}
-		}
-	
-    const isUser = await user.findOne({ where: { id: encryptedPhoneNumber } });
+      if (cacheData !== verifyCode) {
+        throw new Error("인증 번호가 맞지 않습니다.");
+      }
+      cache.delete(phoneNumber);
+    }
+
+    const isUser = await user.findOne({
+      where: {
+        id: encryptedPhoneNumber
+      }
+    });
     if (isUser || phoneNumber === master) {
-      result.accessToken = jwt.sign(
-        { id: encryptedPhoneNumber, type: "A" },
-        jwtSecretKey,
-        {
+      result.accessToken = jwt.sign({
+          id: encryptedPhoneNumber,
+          type: "A"
+        },
+        jwtSecretKey, {
           expiresIn: 60 * 60 * 24 * 30 * 24,
         }
       );
     }
-    cache.delete(phoneNumber);
+
     return result;
   }
 
   const issueTokens = (id) => {
-    const accessToken = jwt.sign({ id: id, type: "A" }, jwtSecretKey, {
+    const accessToken = jwt.sign({
+      id: id,
+      type: "A"
+    }, jwtSecretKey, {
       expiresIn: 60 * 60 * 24 * 30 * 24,
     });
 
-    const refreshToken = jwt.sign({ id: id, type: "R" }, jwtSecretKey, {
+    const refreshToken = jwt.sign({
+      id: id,
+      type: "R"
+    }, jwtSecretKey, {
       expiresIn: 60 * 60 * 24 * 14,
     });
 
-    const result = { accessToken: accessToken, refreshToken: refreshToken };
+    const result = {
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    };
     return result;
   };
 
-  
+
   const createNewUser = async (signUpData) => {
     let transaction = await sequelize.transaction();
     try {
-      const { id, universityId, name, timeTableClasses } = signUpData;
-      const isDuplicated = await user.findOne({where:{id:id}})
-      if(isDuplicated){
+      const {
+        id,
+        universityId,
+        name,
+        timeTableClasses
+      } = signUpData;
+      const isDuplicated = await user.findOne({
+        where: {
+          id: id
+        }
+      })
+      if (isDuplicated) {
         throw new Error("이미 존재하는 유저 입니다.")
       }
       const userData = {
@@ -92,7 +128,9 @@ const AuthService = () => {
         universityId: universityId,
         name: name,
       };
-      const createdUserData = await user.create(userData, { transaction });
+      const createdUserData = await user.create(userData, {
+        transaction
+      });
       const userId = createdUserData.dataValues.userId;
       const timeTableInstance = TimeTableService(userId);
       await timeTableInstance.create(
@@ -100,11 +138,12 @@ const AuthService = () => {
         transaction
       );
       const result = issueTokens(id);
-      await createdUserData.update(
-        { refreshToken: result.refreshToken },
-        { transaction }
-      );
-      
+      await createdUserData.update({
+        refreshToken: result.refreshToken
+      }, {
+        transaction
+      });
+
       await transaction.commit();
       return result;
     } catch (err) {
@@ -114,15 +153,24 @@ const AuthService = () => {
     }
   };
 
-  const deleteUser = async (id) =>{
-    try{
-      await user.destroy({where:{id:id}})
-    }catch(err){
+  const deleteUser = async (id) => {
+    try {
+      await user.destroy({
+        where: {
+          id: id
+        }
+      })
+    } catch (err) {
       throw new Error("유저 삭제 에러");
     }
   }
 
-  return { sendVerifyCode, createNewUser, confirmVerifyCode ,deleteUser};
+  return {
+    sendVerifyCode,
+    createNewUser,
+    confirmVerifyCode,
+    deleteUser
+  };
 };
 
 module.exports = AuthService;
